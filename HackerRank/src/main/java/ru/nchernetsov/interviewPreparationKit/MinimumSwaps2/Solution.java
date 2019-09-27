fbf92de5b356400054efd94385ce69096cfac0b7
nchernetsov <n.chernetsov86@gmail.com>
2019-09-27 20:03:48 +0300

    Заготовка репозитория

---
+++ HackerRank/src/main/java/ru/nchernetsov/interviewPreparationKit/MinimumSwaps2/Solution.java
@@ -0,0 +1,10 @@
+package ru.nchernetsov.interviewPreparationKit.MinimumSwaps2;
+
+public class Solution {
+
+    // Complete the minimumSwaps function below.
+    static int minimumSwaps(int[] arr) {
+
+        return 0;
+    }
+}
