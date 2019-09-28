ccb36c87d8a44427bbe682d8d07739c4ace3d058
ChernetsovNG <n.chernetsov86@gmail.com>
2019-09-28 10:49:08 +0300

    Добавлено описание алгоритма minimum swaps

--- HackerRank/src/main/java/ru/nchernetsov/interviewPreparationKit/MinimumSwaps2/Solution.java
+++ HackerRank/src/main/java/ru/nchernetsov/interviewPreparationKit/MinimumSwaps2/Solution.java
@@ -23,14 +23,21 @@ public class Solution {
             if (checked[elem - 1] || index == elem - 1) {
                 continue;
             }
-            int count = 0;
+            /*
+             Основная идея здесь такая: например, первое число в массиве 7. Оно должно стоять на позиции 6. Но на
+             этом месте стоит 4. 4 должно быть на позиции 3. Но на этом месте стоит 1. 1 должно быть на позиции 1.
+             Но на этом месте 7. Таким образом, мы обнаружили цикл (7 -> 4 -> 1 -> 7). Для упорядочивания цикла нужно
+             сделать n - 1 перестановок в нём, где n - длина цикла. Мы находим все такие циклы и суммируем количества
+             перестановок в них
+             */
+            int cycleLen = 0;
             Integer nextElem = indexElem.get(elem - 1);
             while (!checked[nextElem - 1]) {
                 checked[nextElem - 1] = true;
                 nextElem = indexElem.get(nextElem - 1);
-                count++;
+                cycleLen++;
             }
-            result += (count - 1);
+            result += (cycleLen - 1);
         }
         return result;
     }
