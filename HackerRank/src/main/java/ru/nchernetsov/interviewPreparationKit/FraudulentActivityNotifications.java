c0789b99fd9fbfe92e7b3db7aa23aabc6b83035e
nchernetsov <n.chernetsov86@gmail.com>
2019-10-04 20:19:32 +0300

    FraudulentActivityNotifications: прошли все тесты на HackerRank

--- HackerRank/src/main/java/ru/nchernetsov/interviewPreparationKit/FraudulentActivityNotifications.java
+++ HackerRank/src/main/java/ru/nchernetsov/interviewPreparationKit/FraudulentActivityNotifications.java
@@ -1,35 +1,53 @@
 package ru.nchernetsov.interviewPreparationKit;
 
-import java.util.Arrays;
-
 public class FraudulentActivityNotifications {
+
+    private static final int K = 201;
 
     // Complete the activityNotifications function below.
     static int activityNotifications(int[] expenditure, int d) {
         int nCount = 0;
+
         int n = expenditure.length;
+
         int[] trailing = new int[d];
+        int[] c = new int[K];
+        int[] medianArr = new int[d];
+
+        for (int i = 0; i < d; i++) {
+            trailing[i] = expenditure[i];
+        }
+
+        double m;
         for (int i = d; i < n; i++) {
-            for (int j = i - d; j < i; j++) {
-                trailing[j + d - i] = expenditure[j];
+            if (i == d) {
+                for (int j = 0; j < d; j++) {
+                    c[trailing[j]] += 1;
+                }
+            } else {
+                c[trailing[0]] -= 1;
+                c[expenditure[i - 1]] += 1;
+                trailing[0] = expenditure[i - d];
             }
-            double m = median(trailing);
+
+            // вычисляем медиану
+            int b = 0;
+            for (int elem = 0; elem < K; elem++) {
+                int count = c[elem];
+                for (int f = 0; f < count; f++) {
+                    medianArr[b++] = elem;
+                }
+            }
+            if (d % 2 == 1) {
+                m = medianArr[(d - 1) / 2] * 1.0;
+            } else {
+                m = 0.5 * (medianArr[(d - 1) / 2] + medianArr[d / 2]);
+            }
+
             if (expenditure[i] >= 2 * m) {
                 nCount++;
             }
         }
         return nCount;
-    }
-
-    private static double median(int[] a) {
-        Arrays.sort(a);
-        int len = a.length;
-        double m;
-        if (len % 2 == 1) {
-            m = a[(len - 1) / 2] * 1.0;
-        } else {
-            m = (a[(len - 1) / 2] + a[len / 2]) / 2.0;
-        }
-        return m;
     }
 }
