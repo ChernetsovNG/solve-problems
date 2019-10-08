878f9e89bfde0125781dc458ec559773bf3f49ed
nchernetsov <n.chernetsov86@gmail.com>
2019-10-08 20:58:34 +0300

    MergeSortCountingInversions: solved!

--- HackerRank/src/main/java/ru/nchernetsov/interviewPreparationKit/MergeSortCountingInversions.java
+++ HackerRank/src/main/java/ru/nchernetsov/interviewPreparationKit/MergeSortCountingInversions.java
@@ -9,7 +9,49 @@ public class MergeSortCountingInversions {
     // Сортировка слиянием с подсчётом количества инверсий за O(n x log(n))
 
     private static long mergeInversions(int[] arr) {
-        return 0L;
+        int[] temp = new int[arr.length];
+        return mergeInversions(arr, 0, arr.length - 1, temp);
+    }
+
+    private static long mergeInversions(int[] arr, int leftStart, int rightEnd, int[] temp) {
+        if (leftStart >= rightEnd) {
+            return 0L;
+        }
+        int middle = (leftStart + rightEnd) / 2;
+        long rA = mergeInversions(arr, leftStart, middle, temp);
+        long rB = mergeInversions(arr, middle + 1, rightEnd, temp);
+        long rC = mergeHalvesAndCount(arr, leftStart, rightEnd, temp);
+        return rA + rB + rC;
+    }
+
+    private static long mergeHalvesAndCount(int[] arr, int leftStart, int rightEnd, int[] temp) {
+        int leftEnd = (rightEnd + leftStart) / 2;
+        int rightStart = leftEnd + 1;
+        int size = rightEnd - leftStart + 1;
+
+        int left = leftStart;
+        int right = rightStart;
+        int index = leftStart;
+
+        long count = 0L;
+        while (left <= leftEnd && right <= rightEnd) {
+            if (arr[left] <= arr[right]) {
+                temp[index] = arr[left];
+                left++;
+            } else {  // все элементы левого массива образуют инверсии с правым массивом
+                count += leftEnd - left + 1;
+                temp[index] = arr[right];
+                right++;
+            }
+            index++;
+        }
+
+        System.arraycopy(arr, left, temp, index, leftEnd - left + 1);
+        System.arraycopy(arr, right, temp, index, rightEnd - right + 1);
+
+        System.arraycopy(temp, leftStart, arr, leftStart, size);
+
+        return count;
     }
 
     // Сортировка слиянием
